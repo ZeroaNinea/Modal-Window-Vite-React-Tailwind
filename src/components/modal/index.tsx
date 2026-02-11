4ef07ab4f12604c29234bcf658a7825882a18284
Catgirl <zeroaninea@gmail.com>
2026-02-11 22:35:45 +0400

    the mouse warning fixed

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,4 +1,8 @@
-import { X } from 'feather-icons-react';
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { X, Move } from 'feather-icons-react';
 
 import RippleButton from '../ripple-button';
 
@@ -11,7 +15,72 @@ interface Props {
 }
 
 export default function Modal({ open, onClose, children }: Props) {
-  if (!open) return null;
+  const [isVisible, setIsVisible] = useState(open);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    setDragging(true);
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+  };
+
+  // const handleMouseMove = (e: MouseEvent) => {
+  //   if (!dragging) return;
+
+  //   setPosition({
+  //     x: e.clientX - offset.x,
+  //     y: e.clientY - offset.y,
+  //   });
+  // };
+
+  // const handleMouseUp = () => {
+  //   setDragging(false);
+  // };
+
+  useEffect(() => {
+    if (!dragging) return;
+
+    const handleMouseMove = (e: MouseEvent) => {
+      setPosition({
+        x: e.clientX - offset.x,
+        y: e.clientY - offset.y,
+      });
+    };
+
+    const handleMouseUp = () => {
+      setDragging(false);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [dragging, offset]);
+
+  useEffect(() => {
+    if (open) {
+      setIsVisible(true);
+      setIsClosing(false);
+    } else if (isVisible) {
+      setIsClosing(true);
+      const timeout = setTimeout(() => {
+        setIsVisible(false);
+        setIsClosing(false);
+      }, 300);
+      return () => clearTimeout(timeout);
+    }
+  }, [open, isVisible]);
+
+  if (!isVisible) return null;
 
   return (
     <div
@@ -26,14 +95,41 @@ export default function Modal({ open, onClose, children }: Props) {
       onClick={onClose}
     >
       <div
-        className="relative bg-gray-900 p-8 rounded-xl text-gray-300 min-w-75 fade-in-left"
-        onClick={(e) => e.stopPropagation()}
+        className={`
+            ${isClosing ? 'fade-out-right' : 'fade-in-left'}
+          `}
       >
-        <div className="absolute w-full flex justify-end top-0 left-0 p-2 pointer-events-none">
-          <RippleButton
-            mode="light"
-            onClick={onClose}
-            className="
+        <div
+          style={{
+            transform: `translate(${position.x}px, ${position.y}px)`,
+          }}
+          className={`
+          relative bg-gray-900 p-8 rounded-xl text-gray-300 min-w-75 fade-in-left
+        `}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="absolute w-full flex justify-end items-center top-0 left-0 p-2 pointer-events-none">
+            <div
+              className="
+              relative
+              cursor-move select-none pointer-events-auto
+              w-8 h-8 p-1
+            "
+              onMouseDown={handleMouseDown}
+            >
+              <Move
+                className="
+                absolute
+                top-1/2 left-1/2
+                -translate-x-1/2 -translate-y-1/2
+                w-5 h-5 hover:text-gray-400 transition-all duration-300
+              "
+              />
+            </div>
+            <RippleButton
+              mode="light"
+              onClick={onClose}
+              className="
               cursor-pointer
               w-8 h-8 p-1
               hover:bg-gray-600
@@ -43,11 +139,13 @@ export default function Modal({ open, onClose, children }: Props) {
               transition-all duration-300
               pointer-events-auto
             "
-          >
-            <X className="w-6 h-6" />
-          </RippleButton>
+            >
+              <X className="w-6 h-6" />
+            </RippleButton>
+          </div>
+
+          {children}
         </div>
-        {children}
       </div>
     </div>
   );
