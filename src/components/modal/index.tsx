e47c9540c9f4fbf4080aac4a48a2ecc4dbf9e86a
Catgirl <zeroaninea@gmail.com>
2026-02-11 19:21:25 +0400

    trying to fix the animation

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -10,8 +10,21 @@ export default function Modal({ open, onClose, children }: Props) {
   if (!open) return null;
 
   return (
-    <div className="overlay" onClick={onClose}>
-      <div className="modal" onClick={(e) => e.stopPropagation()}>
+    <div
+      className="
+        fixed
+        inset-0
+        bg-black/60
+        flex
+        justify-center
+        items-center
+      "
+      onClick={onClose}
+    >
+      <div
+        className="bg-gray-900 p-8 rounded-xl text-gray-300 min-w-75"
+        onClick={(e) => e.stopPropagation()}
+      >
         {children}
       </div>
     </div>
